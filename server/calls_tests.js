366f964a69ae61c08f0eb0188e005d2fad1b2b42
urzp <urzp002@mail.ru>
2017-06-29 16:49:43 +0300

    complit data test start to get list of calls

--- server/calls_tests.js
+++ server/calls_tests.js
@@ -1,7 +1,26 @@
-var pilots =  Pilots.findOne()._id;
+var pilots = []
+
+if (Pilots.find().count() === 0){
+	Pilots.insert({
+	name:"Румянцев"
+	})
+	Pilots.insert({
+		name:"Бугров"
+	})
+	Pilots.insert({
+		name:"Назаров"
+	})
+	console.log("pilots added")
+
+
+Pilots.find().forEach(function(pilot){
+    pilots.push({id_pilot:pilot._id});
+})
 
 Flight_calls.insert({
     begin_valid: new Date(2017, 1, 1),
-    end_valid: new Date(2017, 1, 2),
-    data: pilots
-  });
+    end_valid: new Date(2017, 2, 2),
+    flight_call_list: pilots
+ });
+
+}
